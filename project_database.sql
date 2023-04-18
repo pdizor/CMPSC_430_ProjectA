22228a08fd373f946b84b88c9d017babb97038a9
jacobbianco02 <130861136+jacobbianco02@users.noreply.github.com>
2023-04-18 17:50:10 -0400

    Update project_database.sql

--- project_database.sql
+++ project_database.sql
@@ -14,14 +14,14 @@ create table Customer(
 
 create table Loan(
 	loan_ID int,
-	customer_ID int,
+	customer_id int,
 	loan_amount numeric(10, 2) not null,
 	num_payments int not null,
 	interest_rate numeric(2, 2) not null 
 		check (interest_rate > 0 and interest_rate <= 1),
 	amount_paid numeric(10, 2),
-	start_date varchar(10) not null,
-	end_date varchar(10) not null,
+	start_date date not null,
+	end_date date not null,
 	loan_type varchar(10) not null
 		check (loan_type like 'Auto' or 
 		       loan_type like 'Mortgage' or
@@ -36,7 +36,7 @@ create table Auto_Loan(
 	model varchar(20) not null,
 	manufac_year numeric(4, 0) not null,
 	primary key (loan_ID),
-	foreign key (loan_ID) references Loan(loan_ID)
+	foreign key (loan_ID) references Loan ON DELETE CASCADE
 	);
 	
 create table Mortgage_Loan(
@@ -51,13 +51,14 @@ create table Mortgage_Loan(
 	house_price numeric(10, 2)
 		check (house_price >= 0),
 	primary key (loan_ID),
-	foreign key (loan_ID) references Loan(loan_ID)
+	foreign key (loan_ID) references Loan ON DELETE CASCADE
 	);
 	
 create table Personal_Loan(
 	loan_ID int,
 	loan_purpose varchar(250) not null,
 	primary key (loan_ID),
-	foreign key (loan_ID) references Loan(loan_ID)
+	foreign key (loan_ID) references Loan ON DELETE CASCADE
 	);
 	
+# queries, inserts, etc. to follow
